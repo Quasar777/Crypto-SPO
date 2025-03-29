2c5b456789813d1c6fe2cf4215a7675ac253dd03
sarmat <tsaloev.sarmat@gmail.com>
2025-03-29 21:25:19 +0300

    adding asset function adding

--- server/controllers/user-controller.js
+++ server/controllers/user-controller.js
@@ -12,3 +12,25 @@ exports.getPortfolio = async (req, res) => {
     }
 }
 
+exports.addCrypto = async (req, res) => {
+    try {
+        const {userId, id, amount, price} = req.body;
+        const user = await User.findById(userId)
+        if (!user) {
+            return res.status(404).json({ message: 'Пользователь не найден' });
+        }
+        const existingCoin = user.portfolio.find(coin => coin.id === id);
+        if (existingCoin) {
+            existingCoin.amount += amount;
+            existingCoin.price = (existingCoin.price + price) / 2;
+        } else {
+            user.portfolio.push({id, amount, price})
+        }
+
+        await user.save()
+        res.json(user.portfolio);
+
+    } catch(e) {
+        res.status(500).json({ message: error.message });
+    }
+}
