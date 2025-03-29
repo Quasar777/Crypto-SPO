94c8eb737f5a1b7c5b087ec2656c5ab96c1825dd
sarmat <tsaloev.sarmat@gmail.com>
2025-03-29 21:09:22 +0300

    get user portfolio function adding

--- server/controllers/user-controller.js
+++ server/controllers/user-controller.js
@@ -1,2 +1,14 @@
 const User = require('../models/User')
 
+exports.getPortfolio = async (req, res) => {
+    try {
+        const user = await User.findById(req.query.userId);
+        if (!user) {
+            return res.status(404).json({message: 'Пользователь не найден'});
+        }
+        res.json(user.portfolio)
+    } catch(e) {
+        res.status(500).json({message: e.message})
+    }
+}
+
