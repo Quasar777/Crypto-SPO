51dbb32e751680feb77174b44c47a0762a79bcb1
sarmat <tsaloev.sarmat@gmail.com>
2025-03-30 20:35:54 +0300

    fixing removeCrypto user controller method

--- server/controllers/user-controller.js
+++ server/controllers/user-controller.js
@@ -36,48 +36,25 @@ class userController {
             res.json(user.portfolio);
     
         } catch(e) {
-            res.status(500).json({ message: e.message });
+            next(e)
+        }
+    }
+
+    async removeCrypto(req, res, next) {
+        try {
+            const {userId, coinId} = req.body;
+            const user = await User.findById(userId);
+            if (!user) {
+                return res.status(404).json({message: 'Пользователь не найден'})
+            }
+            user.portfolio = user.portfolio.filter(coin => coin.id !== coinId)
+            await user.save()
+    
+        } catch(e) {
+            next(e)
         }
     }
 }
-
-// exports.addCrypto = async (req, res) => {
-//     try {
-//         const {userId, id, amount, price} = req.body;
-//         const user = await User.findById(userId)
-//         if (!user) {
-//             return res.status(404).json({ message: 'Пользователь не найден' });
-//         }
-//         const existingCoin = user.portfolio.find(coin => coin.id === id);
-//         if (existingCoin) {
-//             existingCoin.amount += amount;
-//             existingCoin.price = (existingCoin.price + price) / 2;
-//         } else {
-//             user.portfolio.push({id, amount, price})
-//         }
-
-//         await user.save()
-//         res.json(user.portfolio);
-
-//     } catch(e) {
-//         res.status(500).json({ message: e.message });
-//     }
-// }
-
-// exports.removeCrypto = async (req, res) => {
-//     try {
-//         const {userId, coinId} = req.body;
-//         const user = await User.findById(userId);
-//         if (!user) {
-//             return res.status(404).json({message: 'Пользователь не найден'})
-//         }
-//         user.portfolio = user.portfolio.filter(coin => coin.id !== coinId)
-//         await user.save()
-
-//     } catch(e) {
-//         res.status(500).json({ message: e.message });
-//     }
-// }
 
 
 module.exports = new userController();
