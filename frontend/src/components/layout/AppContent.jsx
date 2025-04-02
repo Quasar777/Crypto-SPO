8b5ade282c3dfc8775681e76991e4dc8098fb23e
sarmat <tsaloev.sarmat@gmail.com>
2025-04-02 17:51:54 +0300

    new info on page: total portfolio profit

--- frontend/src/components/layout/AppContent.jsx
+++ frontend/src/components/layout/AppContent.jsx
@@ -1,4 +1,4 @@
-import { Layout, Typography } from 'antd'
+import { Flex, Layout, Typography } from 'antd'
 import { useCrypto } from '../../context/crypto-context'
 import PortfolioChart from '../PortfolioChart'
 import AssetsTable from '../AssetsTable'
@@ -19,17 +19,35 @@ export default function AppContent() {
     return acc
   }, {})
 
+  const portfolioProfit = assets
+                          .map(asset => asset.totalProfit)
+                          .reduce((acc, val) => (acc += val), 0)
+                          .toFixed(2)
+
   return (
     <Layout.Content style={contentStyle}>
-      <Typography.Title level={3} style={{ textAlign: 'left', color: '#fff' }}>
-        Portfolio:{' '}
-        {assets
-          .map((asset) => asset.amount * cryptoPriceMap[asset.id])
-          .reduce((acc, v) => (acc += v), 0)
-          .toFixed(2)}
-        $
-      </Typography.Title>
-      <PortfolioChart />
+      <Flex justify='space-between'>
+        <Flex  vertical>
+          <Typography.Title level={3} style={{ textAlign: 'left', color: '#fff' }}>
+            Цена портфеля:{' '}
+            {assets
+              .map((asset) => asset.amount * cryptoPriceMap[asset.id])
+              .reduce((acc, v) => (acc += v), 0)
+              .toFixed(2)}
+            $
+          </Typography.Title>
+          <Typography.Title 
+            level={3} 
+            style={{ textAlign: 'left', color: '#fff'}}
+          >    
+              Общая прибыль: {' '}
+              <span style={{color: portfolioProfit >= 0 ? 'lightgreen' : 'red'}}>
+                {portfolioProfit}$
+              </span>
+          </Typography.Title>
+        </Flex>
+        <PortfolioChart />
+      </Flex>
       <AssetsTable />
     </Layout.Content>
   )
